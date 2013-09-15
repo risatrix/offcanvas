cc285abe97d19a67519bf4e75a521e5d290b8f14
Amanda <amanda.n.krauss@gmail.com>
2013-09-14 20:18:14 -0500

    build offcanvas menu

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,6 +1,9 @@
 /*Build the menu*/
   $.fn.offcanvas = function() {
       $('body').children().wrapAll('<div class="content-wrapper">');
+      offContent = '<nav>' + $('#mainMenu').html();   
+      $(offContent).addClass('offcanvas').insertBefore('.content-wrapper');
+      $('.offcanvas ul').append('<li><a href="#">Back</a></li>');
   };
 
   $(window).offcanvas();

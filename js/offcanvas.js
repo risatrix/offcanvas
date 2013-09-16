b5a28e7795c1119ad430f9d4334a715bcbb5b6e3
Amanda <amanda.n.krauss@gmail.com>
2013-09-16 16:11:09 -0500

    create defaults

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,20 +1,22 @@
 /*Build the menu*/
- (function ($, window, document, undefined) {
+ (function ($, window, document, undefined) {	
  
-  $.fn.offcanvas = function(options) {
-	var settings = $.extend({
-        // Default settings
-        target_menu: '#mainMenu',
-    }, options );
+	$.fn.offcanvas = function(options) {
+		var opts = $.extend( {}, $.fn.offcanvas.defaults, options );
 
-  //wrap the content, clone the menu and append the clone to the wrapper	
-  $('body').children().wrapAll('<div class="content-wrapper">');
-  offContent = '<nav>' + $(settings.target_menu).html();   
-  $(offContent).addClass('offcanvas').insertBefore('.content-wrapper');
-  //add a back button to the menu
-  $('.offcanvas ul').append('<li><a href="#" id="close-menu">Back</a></li>');
-  
-  };
+
+		//wrap the content, clone the menu and append the clone to the wrapper	
+		$('body').children().wrapAll('<div class="content-wrapper">');
+		offContent = '<nav>' + $(opts.target_menu).html();   
+		$(offContent).addClass('offcanvas').insertBefore('.content-wrapper');
+		//add a back button to the menu
+		$('.offcanvas ul').append('<li><a href="#" id="close-menu">Back</a></li>');
+
+	};
+
+	$.fn.offcanvas.defaults = {
+	target_menu: "#mainMenu"
+	};	
 
 }(jQuery, window, window.document, undefined));
 
